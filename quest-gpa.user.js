bd788354b91d4a29c72bf8530c717712c5ad9658
Kevin Thai <kvn.thai@gmail.com>
2017-09-19 22:40:28 -0400

    Added in basic layout and styling of components, with filler content.

--- quest-gpa.user.js
+++ quest-gpa.user.js
@@ -20,20 +20,31 @@
     if (window.location.href.includes("https://quest.pecs.uwaterloo.ca/psc/SS/ACADEMIC/SA/c/SA_LEARNER_SERVICES.SS_AA_REPORT1.GBL")) {
 
         // Constructing the elements + styles
-        var sidebar = "<div id='aruu-sidebar'>"+
+        var sidebar = "<div id='aruu-sidebar' class='PSTEXT'>"+
         "<h3 class='PATRANSACTIONTITLE'>GPA Calculator</h3>"+
-        "<button id='calculateGPA' class='SSSBUTTON_ACTIONLINK'>Colour area</button>"+
+        "<div id='gpa-box' class='PSGROUPBOXWBO'>"+
+        "<div class='PSLEVEL2GRIDLABEL'>Cumulative GPA</div>"+
+        "<div class='PSLONGEDITBOX'>XX</div>"+
+        "</div>"+
+        "<button id='calculate-gpa' class='SSSBUTTON_ACTIONLINK'>calculate</button>"+
         "<textarea id='testarea' rows='5' cols='10'></textarea>"+
+        "<p class='select-text'>Include these terms in calculation:</p>"+
+        "<table id='term-table' class='PSEDITBOX_DISPONLY'>"+
+        "<tr><td><input type='checkbox' name='terms' id='test'><label for='test'>Term</label></td></tr>"+
+        "</table>"+
         "</div>";
         var css = "<style>"+
         "#aruu-sidebar{height:300px;width:150px;top:0px;right:50px;border-color:red;position:fixed;text-align:center;}"+
-        "#calculateGPA{margin:auto;width:80px;padding:0;border-right-width:2px;}"+
+        "#gpa-box{margin:10px 15px;}"+
+        "#calculate-gpa{margin:auto;width:80px;padding:0;border-right-width:2px;}"+
+        ".select-text{text-align:left;margin:15px 15px 0 15px;}"+
+        "#term-table{margin:0 15px 15px 15px;}"+
         "</style>";
 
         // Inserting them into the document
         window.document.body.insertAdjacentHTML('beforeend', sidebar);
         window.document.body.insertAdjacentHTML('beforeend', css);
-        window.document.getElementById("calculateGPA").onclick = calculateGPA;
+        window.document.getElementById("calculate-gpa").onclick = calculateGPA;
     }
 
 })();
