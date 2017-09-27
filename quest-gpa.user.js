a686fbaba1a198e106d0775030179d73b0e3daec
Kevin Thai <kvn.thai@gmail.com>
2017-09-26 22:46:27 -0400

    Separated out the transcript scanning and GPA calculation

--- quest-gpa.user.js
+++ quest-gpa.user.js
@@ -9,6 +9,8 @@
 // @version      0.65
 // ==/UserScript==
 
+var terms = [];
+
 (function() {
     'use strict';
 
@@ -29,11 +31,13 @@
             "<button id='read-transcript' class='sidebar-button SSSBUTTON_ACTIONLINK'>1. read transcript</button>"+
             "<button id='calculate-gpa' class='sidebar-button SSSBUTTON_ACTIONLINK'>2. calculate</button>"+
             "<textarea id='testarea' rows='5' cols='10'></textarea>"+
+            "<form id='term-selection'>"+
             "<p class='select-text'>Include these terms in calculation (click calculate to update GPA):</p>"+
             "<table class='term-table PSEDITBOX_DISPONLY'>"+
             "<tbody id='term-table-body'>"+
             "</tbody>"+
             "</table>"+
+            "</form>"+
             "</div>";
         let css = "<style>"+
             ".aruu-sidebar{max-height:500px;width:220px;top:10px;right:50px;position:fixed;text-align:center;"+
@@ -48,12 +52,13 @@
         // Inserting them into the document
         window.document.body.insertAdjacentHTML('beforeend', sidebar);
         window.document.body.insertAdjacentHTML('beforeend', css);
+        window.document.getElementById("read-transcript").onclick = readTranscript;
         window.document.getElementById("calculate-gpa").onclick = calculateGPA;
     }
 
 })();
 
-function calculateGPA() {
+function readTranscript() {
     var transcriptDiv = window.document.getElementById("PrintTranscript"),
         testarea = window.document.getElementById("testarea");
 
@@ -73,7 +78,7 @@ function calculateGPA() {
     }
 
     // Parse the filtered lines into a data structure to store the term names and grades
-    var terms = [];
+    terms = [];
     var term = {desc:"", courses:[]};
     for (let i=0; i<transcript_raw.length; i++) {
         if (result = /^<b>(\w* \d{4})\s*(\d\w)/.exec(transcript_raw[i])) {
@@ -101,12 +106,14 @@ function calculateGPA() {
     window.document.getElementById("term-table-body").innerHTML = "";
     // Get term names and create checkbox controls to select terms included
     for (let i=0; i<terms.length; i++) {
-        let checkbox = "<tr><td><input type='checkbox' name='term" + i +
-            "' id='term" + i + "'><label for='term" + i + "'>" +
+        let checkbox = "<tr><td><input type='checkbox' id='term" + i + "' checked>"+
+            "<label for='term" + i + "'>"+
             terms[i].desc + "</label></td></tr>";
         window.document.getElementById("term-table-body").insertAdjacentHTML('beforeend', checkbox);
     }
+}
 
+function calculateGPA() {
     // Take the transcript and calculate the cumulative GPA
     var gpa = 0, gpa_weight = 0;
     for (let i=0; i<terms.length; i++) {
@@ -116,8 +123,6 @@ function calculateGPA() {
         }
     }
     gpa /= gpa_weight;
-    window.document.getElementById("gpa-box").innerHTML = gpa.toFixed(2);
-
-    console.log(terms);
     console.log(gpa);
+    window.document.getElementById("gpa-box").innerHTML = gpa.toFixed(2);
 }
